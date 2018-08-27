780fa33712b7da7ccfb12490b8d7fc2c6b9fed3b
jon.xia <xiajia@vip.qq.com>
2018-08-27 18:43:29 +0800

    增加icons支持，完善按钮显示

--- src/features/admin/TopPanel.js
+++ src/features/admin/TopPanel.js
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from './redux/actions';
 import {Grid, Button, Menu, MenuItem} from '@material-ui/core';
+import {withRouter} from 'react-router-dom'
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown'
 
 export class TopPanel extends Component {
   static propTypes = {
@@ -17,12 +19,16 @@ export class TopPanel extends Component {
     }
     this.handleClickUserMenu = this.handleClickUserMenu.bind(this)
     this.handleClose = this.handleClose.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
   }
   handleClickUserMenu(event) {
     this.setState({ anchorEl: event.currentTarget })
   }
   handleClose() {
     this.setState({anchorEl: null})
+  }
+  handleLogout() {
+    this.props.history.push('/logout')
   }
   render() {
     const { anchorEl } = this.state
@@ -32,7 +38,7 @@ export class TopPanel extends Component {
           <Grid item xs={4} className="admin-top-panel-title"><h3>{this.props.admin.sideSelected}</h3></Grid>
           <Grid item xs={6} className="admin-top-panel-menu"></Grid>
           <Grid item xs={2} className="admin-top-panel-right">
-            <Button variant="flat" aria-owns="simple-menu" aria-haspopup="true" onClick={this.handleClickUserMenu}>超级管理员</Button>
+            <Button variant="flat" aria-owns="simple-menu" aria-haspopup="true" onClick={this.handleClickUserMenu}>超级管理员 <ArrowDropDown></ArrowDropDown></Button>
             <Menu
               id="simple-menu"
               anchorEl={anchorEl}
@@ -41,7 +47,7 @@ export class TopPanel extends Component {
             >
               <MenuItem onClick={this.handleClose}>个人资料</MenuItem>
               <MenuItem onClick={this.handleClose}>修改密码</MenuItem>
-              <MenuItem onClick={this.handleClose}>退出系统</MenuItem>
+              <MenuItem onClick={this.handleLogout}>退出系统</MenuItem>
             </Menu>
           </Grid>
         </Grid>
@@ -67,4 +73,4 @@ function mapDispatchToProps(dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(TopPanel);
+)(withRouter(TopPanel));
